d9ed73f5ddc88eb419f8704e235803abfec63fa6
jacek.sawoszczuk <jacek.sawoszczuk@gmail.com>
2019-10-15 07:39:56 +0200

    update

--- presentation.js
+++ presentation.js
@@ -3,45 +3,70 @@ const slideCount = slides.length;
 const startingSlideIndex = getCurrentSlideIndex();
 let currentSlideIndex = startingSlideIndex;
 
+let hands = document.getElementsByClassName("hand");
+
 const options = {
     startWithFirstSlideShown: true,
+    wrapSlides: false,
+    useAnimations: true,
+    animations: {
+        animationSpeed: '4s',
+        randomise: true,
+        in: ['in_1', 'in_2'],
+        out: ['out_1', 'out_2'],
+        fixedIn: 'in_1',
+        fixedOut: 'out_2',
+        action: 'out_2'
+    }
 }
 
 hideSlides(slides);
-
-
-// setting css properties
-// document.documentElement.style.setProperty('--theme-darkness', val);
-
+document.documentElement.style.setProperty('--animation-speed', options.animations.animationSpeed);
 
 // ----------------------------
 
 function goToPreviousSlide() {
-    console.log('prev');
-    hideCurrentSlide();
-    decrementCurrentSlideIndex();
-    showCurrentSlide();
-    updateFragment();
+    if (currentSlideIndex > 0 || options.wrapSlides) {
+        hideSlide(slides[currentSlideIndex]);
+        decrementCurrentSlideIndex();
+        // tweakCurrentSlide(); // TODO
+        showSlide(slides[currentSlideIndex]);
+        updateFragment();
+    }
 }
 
 function goToNextSlide() {
-    console.log('next');
-    hideCurrentSlide();
-    incrementCurrentSlideIndex();
-    showCurrentSlide();
-    updateFragment();
+    if (currentSlideIndex < slideCount - 1 || options.wrapSlides) {
+        hideSlide(slides[currentSlideIndex]);
+        incrementCurrentSlideIndex();
+        // tweakCurrentSlide(); // TODO
+        showSlide(slides[currentSlideIndex]);
+        updateFragment();
+    }
 }
 
-function updateFragment() {
-    window.location.hash = currentSlideIndex;
+function showSlide(slide) {
+    slide.classList.remove('off');
+    if (options.useAnimations) {
+        let animClass = options.animations.randomise
+            ? options.animations.in[getRandomInt(options.animations.in.length)]
+            : options.animations.fixedIn;
+        slide.classList.add(animClass);
+    }
 }
 
-function showCurrentSlide() {
-    slides[currentSlideIndex].classList.remove("off");
-}
+function hideSlide(slide) {
+    slide.classList.remove('off');
+    if (options.useAnimations) {
 
-function hideCurrentSlide() {
-    slides[currentSlideIndex].classList.add("off");
+        slide.appendChild(hands[0]);
+        hands[0].classList.add('move-in');
+
+        let animClass = options.animations.randomise
+            ? options.animations.out[getRandomInt(options.animations.out.length)]
+            : options.animations.fixedOut;
+        slide.classList.add(animClass);
+    }
 }
 
 function incrementCurrentSlideIndex() {
@@ -57,7 +82,7 @@ function hideSlides(slides) {
         if (options.startWithFirstSlideShown && startingSlideIndex === i) {
             continue;
         }
-        slides[i].classList.toggle("off");
+        slides[i].classList.toggle('off');
 
     }
 }
@@ -71,21 +96,51 @@ function getCurrentSlideIndex() {
     }
 }
 
+function updateFragment() {
+    window.location.hash = currentSlideIndex;
+}
+
 function getFragment() {
     return window.location.hash.substr(1);
 }
 
+function action() {
+    slides[currentSlideIndex].classList.toggle(options.animations.action);
+}
+
 document.addEventListener('keyup', function (event) {
     switch (event.key) {
-        case "ArrowLeft":
+        case 'ArrowLeft':
             goToPreviousSlide();
             break;
-        case "ArrowRight":
+        case 'ArrowRight':
             goToNextSlide();
             break;
-        case "ArrowUp":
+        case 'ArrowUp':
             break;
-        case "ArrowDown":
+        case 'ArrowDown':
+            break;
+        case ' ':
+            action();
             break;
     }
 });
+
+document.addEventListener('animationend', (e) => {
+    if (e.animationName.startsWith('out')) {
+        e.target.classList.add('off');
+        e.target.classList.remove(e.animationName);
+        let hand = e.target.getElementsByClassName('hand')[0];
+        if(hand) {
+            hand.classList.remove('move-in');
+        }
+    }
+    if (e.animationName.startsWith('in')) {
+        e.target.classList.remove('off');
+        e.target.classList.remove(e.animationName);
+    }
+});
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
